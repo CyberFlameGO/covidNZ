9b500f92eac232f34e49769499979fa1def5764a
Aaron Lowe <cyberflameu@gmail.com>
2022-02-26 11:44:52 +1300

    erm

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 ML model for predicting NZ COVID cases
 """
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 # import tensorflow as tf
 # from tensorflow import keras
@@ -17,6 +17,13 @@ def main():
     df = pd.read_csv('covid.csv', usecols=[1])
     dataset = df.values
     dataset = df.astype('float32')
+    plt.plot(dataset)
+    plt.show()
+
+    train_size = int(len(dataset) * 0.67)
+    test_size = len(dataset) - train_size
+    train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+    print(len(train), len(test))
 
 
 if __name__ == '__main__':
