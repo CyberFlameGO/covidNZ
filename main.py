8b9447124d8eaa10140127c40de2e9742699dac0
Aaron Lowe <cyberflameu@gmail.com>
2022-02-25 18:09:22 +1300

    pog

--- main.py
+++ main.py
@@ -2,8 +2,22 @@
 """
 ML model for predicting NZ COVID cases
 """
-import tensorflow as tf
-from tensorflow import keras
+
+import matplotlib as plt
+import pandas as pd
+# import tensorflow as tf
+# from tensorflow import keras
+import numpy as np
 
 
+def main():
+    """
+    Main function
+    """
+    df = pd.read_csv('covid.csv', usecols=[1])
+    dataset = df.values
+    dataset = df.astype('float32')
 
+
+if __name__ == '__main__':
+    main()
